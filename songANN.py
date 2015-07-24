d8b2c39505aa029055abe3bc25080b89ba3529bb
IanWoods1993 <ianwoods1993@gmail.com>
2015-07-23 22:35:24 -0700

    probably ought to do this right now. Working on initializing the network and figuring out the structure

--- songANN.py
+++ songANN.py
@@ -18,17 +18,37 @@ def getSpectrogram(songPath):
 	input_data = read(songPath)
 	audio = input_data[1]
 	spectrogram = plt.specgram(audio)
-	print(spectrogram[0]) #i don't really understand what the numbers here mean
+	#plt.show()
+	#print(spectrogram[0][0]) #i don't really understand what the numbers here mean
+	# the ... are just a number representation issue
 	return(spectrogram)
 
+#spectrogram is 812 x 612 pixels = 496944 pixels
+#a node for every pixel is a shitty idea -> too big
+
+def getAudioFromSpectrogram(spectrogram):
+	try:
+    #change the file's name and format
+		image_file = 'image.png'
+		fin = open(image_file, "rb") #binary read
+		data = fin.read()
+		fin.close()
+	except IOError:
+		print("Image file %s not found" % imageFile)
+		raise SystemExit
+
+	return(audio)
+
 def initializeNeuralNet():
+	neuralNet = FeedForwardNetwork()
 	return(neuralNet)
 
 def main():
-	#song = open(getSong())
-	#songPath = song.name
-	songPath = "/home/ian/songANN/COTD.wav" #just to skip the box
+	song = open(getSong())
+	songPath = song.name
+	#songPath = "/home/ian/songANN/COTD.wav" #just to skip the box
 	spectrogram = getSpectrogram(songPath)
+	#here, call getAudioFromSpectrogram with the right args, etc.
 	neuralNet = initializeNeuralNet()
 	#spectrogram = input for the neural network
 	return[0]
